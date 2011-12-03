fad9dad8c8c0fdf2fec59e8e6cdea2b07737824d
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-03 00:41:29 +0000

    Initial version.

---
+++ trunk/example/src/main/java/org/rivalry/example/dogbreed/DogBreedsParser.java
@@ -0,0 +1,39 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
+package org.rivalry.example.dogbreed;
+
+import org.openqa.selenium.WebElement;
+import org.rivalry.core.datacollector.ValueStringParser;
+
+/**
+ * Provides a value string parser implementation to handle dog breed
+ * information.
+ */
+public class DogBreedsParser implements ValueStringParser
+{
+    @Override
+    public Double parse(final WebElement webElement)
+    {
+        Double answer = null;
+
+        if (webElement != null)
+        {
+            final String valueString = webElement.getAttribute("class");
+
+            final int index = valueString.lastIndexOf('-');
+            if (index >= 0)
+            {
+                final String value = valueString.substring(index + 1);
+                answer = Double.parseDouble(value);
+            }
+        }
+
+        return answer;
+    }
+}
