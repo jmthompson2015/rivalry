033e95897b12a13f4fe76e87e6459e34ff9f3727
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-03 00:41:52 +0000

    Initial version.

---
+++ trunk/example/src/main/java/org/rivalry/example/skilldemand/SkillDemandReporterMain.java
@@ -0,0 +1,159 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
+package org.rivalry.example.skilldemand;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.rivalry.core.model.RivalryData;
+import org.rivalry.core.model.RivalryDataReader;
+import org.rivalry.core.reporter.DefaultReporter;
+import org.rivalry.core.reporter.Reporter;
+
+/**
+ * Provides a reporter for skill demand.
+ */
+public class SkillDemandReporterMain
+{
+    /**
+     * Application method.
+     * 
+     * @param args Application arguments.
+     * 
+     * @throws IOException if there is an I/O problem.
+     * @throws ParseException if there is a parsing problem.
+     */
+    public static final void main(final String[] args) throws IOException,
+            ParseException
+    {
+        final Options options = createOptions();
+        final CommandLineParser parser = new PosixParser();
+        final CommandLine commandLine = parser.parse(options, args);
+
+        if (commandLine.hasOption('h'))
+        {
+            printHelp(options);
+        }
+        else
+        {
+            final String inputFile = determineInputFile(commandLine);
+            System.out.println("inputFile = [" + inputFile + "]");
+
+            final SkillDemandReporterMain main = new SkillDemandReporterMain();
+            final RivalryData rivalryData = main.readRivalryData(inputFile);
+
+            final Writer writer = new StringWriter();
+            final Reporter reporter = new DefaultReporter(
+                    DefaultReporter.ReportStyle.CRITERION_CANDIDATE,
+                    rivalryData, writer);
+            reporter.writeReport();
+
+            System.out.println(writer.toString());
+            final File file = new File("Reports.txt");
+            reporter.writeToFile(file);
+        }
+    }
+
+    /**
+     * @return application command line options.
+     */
+    private static final Options createOptions()
+    {
+        final Options answer = new Options();
+
+        final Option help = new Option("h", "print this message");
+
+        OptionBuilder.withArgName("file");
+        OptionBuilder.hasArg();
+        OptionBuilder.withDescription("Skill demand data file");
+
+        final Option inputFile = OptionBuilder.create("f");
+
+        // OptionBuilder.withArgName("directory");
+        // OptionBuilder.hasArg();
+        // OptionBuilder.withDescription("output directory");
+        //
+        // final Option directory = OptionBuilder.create("d");
+
+        answer.addOption(help);
+        answer.addOption(inputFile);
+        // answer.addOption(directory);
+
+        return answer;
+    }
+
+    /**
+     * @param commandLine Command line.
+     * 
+     * @return input file.
+     */
+    private static final String determineInputFile(final CommandLine commandLine)
+    {
+        String answer = "SkillDemandDataCollector.xml";
+
+        final char option = 'f';
+
+        if (commandLine.hasOption(option))
+        {
+            answer = commandLine.getOptionValue(option);
+        }
+
+        return answer;
+    }
+
+    /**
+     * Print the help text.
+     * 
+     * @param options Command line options.
+     */
+    private static final void printHelp(final Options options)
+    {
+        // Automatically generate the help statement.
+        final HelpFormatter formatter = new HelpFormatter();
+        formatter
+                .printHelp(
+                        "java [-cp <classpath>] org.rivalry.example.skilldemand.SkillDemandReporterMain",
+                        options);
+    }
+
+    /**
+     * @param inputFile Input file.
+     * 
+     * @return rivalry data.
+     */
+    private RivalryData readRivalryData(final String inputFile)
+    {
+        RivalryData answer = null;
+
+        try
+        {
+            final RivalryDataReader rivalryDataReader = new RivalryDataReader();
+            final FileReader reader = new FileReader(inputFile);
+            answer = rivalryDataReader.read(reader);
+        }
+        catch (final FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+        return answer;
+    }
+}
