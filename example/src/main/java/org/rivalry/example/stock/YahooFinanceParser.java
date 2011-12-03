64fb96b94814a65c9537a86a610b8f5cd4f96d1f
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-03 00:50:55 +0000

    Initial version.

---
+++ example/src/main/java/org/rivalry/example/stock/YahooFinanceParser.java
@@ -0,0 +1,73 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
+package org.rivalry.example.stock;
+
+import org.apache.commons.lang.StringUtils;
+import org.openqa.selenium.WebElement;
+import org.rivalry.core.datacollector.ValueStringParser;
+
+/**
+ * Provides a parser for double values.
+ */
+public class YahooFinanceParser implements ValueStringParser
+{
+    @Override
+    public Double parse(final WebElement webElement)
+    {
+        Double answer = null;
+
+        if (webElement != null)
+        {
+            final String valueString = webElement.getText();
+
+            if (StringUtils.isNotEmpty(valueString))
+            {
+                final String myString = valueString.trim();
+
+                if (myString.endsWith("%"))
+                {
+                    answer = parseDoubleOnly(myString.substring(0,
+                            myString.length() - 1)) / 100.0;
+                }
+                else if (myString.endsWith("B"))
+                {
+                    answer = parseDoubleOnly(myString.substring(0,
+                            myString.length() - 1)) * 1000000000;
+                }
+                else
+                {
+                    answer = parseDoubleOnly(myString);
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * @param valueString Value string.
+     * 
+     * @return a double parsed from the given parameter, if possible.
+     */
+    private Double parseDoubleOnly(final String valueString)
+    {
+        Double answer = null;
+
+        try
+        {
+            answer = Double.parseDouble(valueString);
+        }
+        catch (final NumberFormatException ignore)
+        {
+            // Nothing to do.
+        }
+
+        return answer;
+    }
+}
