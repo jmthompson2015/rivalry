f9e034f3da4601e80cf2b3ecf992674d4e8e13e7
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-04 00:36:40 +0000

    Added file header comment.

--- core/src/main/java/org/rivalry/core/model/RivalryDataWriter.java
+++ core/src/main/java/org/rivalry/core/model/RivalryDataWriter.java
@@ -1,3 +1,11 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
 package org.rivalry.core.model;
 
 import java.beans.XMLEncoder;
