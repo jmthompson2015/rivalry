6df05a2389a37ae1983a860994db4365efe0cd59
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-04 18:57:03 +0000

    Added a second report style.

--- core/src/main/java/org/rivalry/core/reporter/DefaultReporter.java
+++ core/src/main/java/org/rivalry/core/reporter/DefaultReporter.java
@@ -1,3 +1,11 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
 package org.rivalry.core.reporter;
 
 import java.io.File;
@@ -19,8 +27,14 @@ import org.rivalry.core.model.RivalryData;
  */
 public class DefaultReporter implements Reporter
 {
-    /** Label width. */
-    private static final int LABEL_WIDTH = 10;
+    /** Report style. */
+    public enum ReportStyle
+    {
+        /** Candidate then criterion order. */
+        CANDIDATE_CRITERION,
+        /** Criterion then candidate order. */
+        CRITERION_CANDIDATE;
+    }
 
     /** Column width. */
     private static final int COLUMN_WIDTH = 8;
@@ -28,9 +42,15 @@ public class DefaultReporter implements Reporter
     /** Horizontal rule. */
     private static final String HORIZONTAL_RULE = "-------------------";
 
+    /** Label width. */
+    private static final int LABEL_WIDTH = 10;
+
     /** Line separator. */
     private static final String LINE_SEPARATOR = System
             .getProperty("line.separator");
+
+    /** Report style. */
+    private final ReportStyle _reportStyle;
 
     /** Rivalry data. */
     private final RivalryData _rivalryData;
@@ -41,11 +61,14 @@ public class DefaultReporter implements Reporter
     /**
      * Construct this object with the given parameters.
      * 
+     * @param reportStyle Report style.
      * @param rivalryData Rivalry data.
      * @param writer Writer.
      */
-    public DefaultReporter(final RivalryData rivalryData, final Writer writer)
+    public DefaultReporter(final ReportStyle reportStyle,
+            final RivalryData rivalryData, final Writer writer)
     {
+        _reportStyle = reportStyle;
         _rivalryData = rivalryData;
         _writer = writer;
     }
@@ -53,23 +76,14 @@ public class DefaultReporter implements Reporter
     @Override
     public void writeReport()
     {
-        final List<Criterion> criteria = _rivalryData.getCriteriaList();
-
-        for (final Criterion criterion : criteria)
+        switch (_reportStyle)
         {
-            writeLine("Criterion: " + criterion.getName());
-
-            writeTitleRow();
-
-            // Sort the candidates by their rating for this criterion.
-            final List<Candidate> candidates = createSortedList(criterion);
-
-            for (final Candidate candidate : candidates)
-            {
-                final Double rating = candidate.getRating(criterion);
-                writeLine(String.format("%-" + LABEL_WIDTH + "s %,"
-                        + COLUMN_WIDTH + ".0f", candidate.getName(), rating));
-            }
+        case CRITERION_CANDIDATE:
+            writeCriterionCandidateReport();
+            break;
+        case CANDIDATE_CRITERION:
+            writeCandidateCriterionReport();
+            break;
         }
     }
 
@@ -124,6 +138,69 @@ public class DefaultReporter implements Reporter
         catch (final IOException ignore)
         {
             // Nothing to do.
+        }
+    }
+
+    /**
+     * Write a report in candidate then criterion order.
+     */
+    private void writeCandidateCriterionReport()
+    {
+        // FIXME: determine maximum criterion name length
+        final int labelWidth = 30;
+        // FIXME: determine maximum candidate name length
+        final int columnWidth = 15;
+
+        final String labelFormat = "%-" + labelWidth + "s";
+        final String columnFormat0 = " %" + columnWidth + "s";
+        final String columnFormat1 = " %," + columnWidth + ".0f";
+
+        final List<Candidate> candidates = _rivalryData.getCandidatesList();
+        final List<Criterion> criteria = _rivalryData.getCriteriaList();
+
+        writeLine(String.format(labelFormat + columnFormat0 + columnFormat0
+                + columnFormat0, "Candidates", candidates.get(0).getName(),
+                candidates.get(1).getName(), candidates.get(2).getName()));
+
+        for (final Criterion criterion : criteria)
+        {
+            final int size = candidates.size();
+            final Double[] ratings = new Double[size];
+            String format = "";
+            for (int i = 0; i < size; i++)
+            {
+                final Candidate candidate = candidates.get(i);
+                ratings[i] = candidate.getRating(criterion);
+                format += columnFormat1;
+            }
+
+            writeLine(String.format(labelFormat + format, criterion.getName(),
+                    ratings[0], ratings[1], ratings[2]));
+        }
+    }
+
+    /**
+     * Write a report in criterion then candidate order.
+     */
+    private void writeCriterionCandidateReport()
+    {
+        final List<Criterion> criteria = _rivalryData.getCriteriaList();
+
+        for (final Criterion criterion : criteria)
+        {
+            writeLine("Criterion: " + criterion.getName());
+
+            writeTitleRow();
+
+            // Sort the candidates by their rating for this criterion.
+            final List<Candidate> candidates = createSortedList(criterion);
+
+            for (final Candidate candidate : candidates)
+            {
+                final Double rating = candidate.getRating(criterion);
+                writeLine(String.format("%-" + LABEL_WIDTH + "s %,"
+                        + COLUMN_WIDTH + ".0f", candidate.getName(), rating));
+            }
         }
     }
 
