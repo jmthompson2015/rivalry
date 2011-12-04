c2e4b583fcdb8e63024648b97beaf181c4d29c76
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-04 18:55:46 +0000

    Added a name string parser. Lots of other changes.

--- trunk/core/src/main/java/org/rivalry/core/datacollector/DefaultDataCollector.java
+++ trunk/core/src/main/java/org/rivalry/core/datacollector/DefaultDataCollector.java
@@ -11,81 +11,27 @@ package org.rivalry.core.datacollector;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.Validate;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.rivalry.core.model.Candidate;
 import org.rivalry.core.model.Category;
 import org.rivalry.core.model.Criterion;
 import org.rivalry.core.model.RivalryData;
 import org.rivalry.core.util.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provides a default implementation of a data collector.
  */
 public class DefaultDataCollector implements DataCollector
 {
-    /**
-     * Provides an expected condition implementation which looks for a visible
-     * element.
-     */
-    class VisibilityOfElementLocated implements ExpectedCondition<Boolean>
-    {
-        /** Find condition. */
-        private By _findCondition;
-
-        /** Start time. */
-        private Long _start;
-
-        /** Text. */
-        private String _text;
-
-        /**
-         * @param by Find condition.
-         * @param text Text.
-         */
-        VisibilityOfElementLocated(By by, String text)
-        {
-            Validate.notNull(by);
-            Validate.notEmpty(text);
-
-            _findCondition = by;
-            _text = text;
-            System.out.println("text = [" + text + "]");
-        }
-
-        @Override
-        public Boolean apply(WebDriver driver)
-        {
-            if (_start == null)
-                _start = System.currentTimeMillis();
-
-            Boolean answer = Boolean.FALSE;
-
-            WebElement element = driver.findElement(_findCondition);
-
-            if (element != null)
-            {
-                // String myText = element.getText();
-                // long elapsed = System.currentTimeMillis() - _start;
-                // System.out.println(elapsed + "\tmyText = [" + myText + "]");
-
-                if (_text.equals(element.getText()))
-                {
-                    answer = Boolean.TRUE;
-                    // System.out.println(driver.getPageSource());
-                }
-            }
-
-            return answer;
-        }
-    }
+    /** Logger. */
+    static final Logger LOGGER = LoggerFactory
+            .getLogger(DefaultDataCollector.class);
 
     /** Category provider. */
     private final Provider<Category> _categoryProvider;
@@ -93,47 +39,29 @@ public class DefaultDataCollector implements DataCollector
     /** Criterion provider. */
     private final Provider<Criterion> _criterionProvider;
 
+    /** Name string parser. */
+    private final NameStringParser _nameStringParser;
+
     /** Value string parser. */
     private final ValueStringParser _valueStringParser;
 
     /**
      * Construct this object with the given parameter.
      * 
+     * @param nameStringParser Name string parser.
      * @param valueStringParser Value string parser.
      * @param categoryProvider Category provider.
      * @param criterionProvider Criterion provider.
      */
-    public DefaultDataCollector(final ValueStringParser valueStringParser,
+    public DefaultDataCollector(final NameStringParser nameStringParser,
+            final ValueStringParser valueStringParser,
             final Provider<Category> categoryProvider,
             final Provider<Criterion> criterionProvider)
     {
+        _nameStringParser = nameStringParser;
         _valueStringParser = valueStringParser;
         _categoryProvider = categoryProvider;
         _criterionProvider = criterionProvider;
-    }
-
-    @Override
-    public void fetchData(DCSpec dcSpec, String username, String password,
-            RivalryData rivalryData)
-    {
-        final long start = System.currentTimeMillis();
-
-        WebDriver webDriver = createWebDriver();
-
-        try
-        {
-            for (final Candidate candidate : rivalryData.getCandidatesList())
-            {
-                fetchData(webDriver, dcSpec, rivalryData, candidate);
-            }
-        }
-        catch (final Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        final long end = System.currentTimeMillis();
-        logTiming("0 fetchData()", start, end);
     }
 
     @Override
@@ -152,6 +80,30 @@ public class DefaultDataCollector implements DataCollector
         logTiming("1 fetchData()", start, end);
     }
 
+    @Override
+    public void fetchData(final DCSpec dcSpec, final String username,
+            final String password, final RivalryData rivalryData)
+    {
+        final long start = System.currentTimeMillis();
+
+        final WebDriver webDriver = createWebDriver();
+
+        try
+        {
+            for (final Candidate candidate : rivalryData.getCandidatesList())
+            {
+                fetchData(webDriver, dcSpec, rivalryData, candidate);
+            }
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        final long end = System.currentTimeMillis();
+        logTiming("0 fetchData()", start, end);
+    }
+
     /**
      * @param webDriver Web driver.
      * @param dcSpec Data collector specification.
@@ -164,20 +116,12 @@ public class DefaultDataCollector implements DataCollector
     {
         final long start = System.currentTimeMillis();
 
-        // disableWebDriverLogging();
-
         // And now use this to visit a web page.
         final String url = candidate.getPage();
         if (StringUtils.isNotEmpty(url))
         {
             System.out.println("Accessing URL " + url);
             webDriver.get(url);
-
-            // Check the title of the page.
-            // System.out.println("Page title is: " + webDriver.getTitle());
-            // WebElement element = webDriver.findElement(By.tagName("title"));
-            // System.out.println("title element = [" + element.getText() +
-            // "]");
         }
 
         for (final DCSelector selector : dcSpec.getSelectors())
@@ -186,13 +130,13 @@ public class DefaultDataCollector implements DataCollector
             switch (type)
             {
             case CLICK:
-                DCSelector childSelector = selector.getSelectors().get(0);
-                SelectorType childType = childSelector.getType();
-                String childValue = childSelector.getValue();
-                System.out.println("childType = " + childType
-                        + " childValue = " + childValue);
+                final DCSelector childSelector = selector.getSelectors().get(0);
+                final SelectorType childType = childSelector.getType();
+                final String childValue = childSelector.getValue();
+                LOGGER.debug("childType = " + childType + " childValue = "
+                        + childValue);
                 childType.findElement(webDriver, childValue).click();
-                System.out.println("2 Page title is: " + webDriver.getTitle());
+                LOGGER.debug("2 Page title is: " + webDriver.getTitle());
                 break;
 
             case SUBMIT:
@@ -211,7 +155,7 @@ public class DefaultDataCollector implements DataCollector
         }
 
         final long end = System.currentTimeMillis();
-        logTiming("1 fetchData()", start, end);
+        logTiming("2 fetchData()", start, end);
     }
 
     /**
@@ -245,45 +189,28 @@ public class DefaultDataCollector implements DataCollector
     }
 
     /**
-     * Disable Selenium WebDriver logger.
-     */
-    // private void disableWebDriverLogging()
-    // {
-    // final Logger logger = Logger.getLogger("");
-    // logger.setLevel(Level.OFF);
-    // }
-
-    /**
      * @param parent Parent web element.
      * @param selector0 Data collection selector.
      * @param size Size.
      * 
      * @return a list of names.
      */
-    private List<String> createNames(WebElement parent, DCSelector selector0,
-            int size)
+    private List<String> createNames(final WebElement parent,
+            final DCSelector selector0, final int size)
     {
-        List<String> answer = new ArrayList<String>();
+        final List<String> answer = new ArrayList<String>();
 
-        if (selector0.getType() == SelectorType.LITERAL)
+        LOGGER.debug("parent = " + parent);
+        final List<WebElement> elements0 = selector0.getType().findElements(
+                parent, selector0.getValue());
+
+        for (int i = 0; i < size; i++)
         {
-            String value = selector0.getValue();
-
-            for (int i = 0; i < size; i++)
-            {
-                answer.add(value);
-            }
-        }
-        else
-        {
-            final List<WebElement> elements0 = selector0.getType()
-                    .findElements(parent, selector0.getValue());
-
-            for (int i = 0; i < size; i++)
-            {
-                final String name = elements0.get(i).getText();
-                answer.add(name);
-            }
+            LOGGER.debug("elements0.get(" + i + ") = "
+                    + elements0.get(i).getTagName() + " ["
+                    + elements0.get(i).getText() + "]");
+            final String name = _nameStringParser.parse(elements0.get(i));
+            answer.add(name);
         }
 
         return answer;
@@ -296,7 +223,8 @@ public class DefaultDataCollector implements DataCollector
     {
         final WebDriver answer = new HtmlUnitDriver();
 
-        final Logger logger = Logger.getLogger("");
+        final java.util.logging.Logger logger = java.util.logging.Logger
+                .getLogger("");
         logger.setLevel(Level.OFF);
 
         if (answer instanceof HtmlUnitDriver)
@@ -341,6 +269,7 @@ public class DefaultDataCollector implements DataCollector
     private void logTiming(final String title, final long start, final long end)
     {
         System.out.println(title + " \telapsed: " + (end - start) + " ms");
+        LOGGER.info(title + " \telapsed: " + (end - start) + " ms");
     }
 
     /**
@@ -355,7 +284,7 @@ public class DefaultDataCollector implements DataCollector
             final Candidate candidate, final WebElement parent,
             final DCSelector selector)
     {
-        System.out.println("selector = " + selector.getType() + " "
+        LOGGER.debug("selector = " + selector.getType() + " "
                 + selector.getValue());
 
         final WebElement child = selector.getType().findElement(parent,
@@ -363,8 +292,8 @@ public class DefaultDataCollector implements DataCollector
 
         if (selector.getSelectors().isEmpty())
         {
-            System.out.println("parent.getText() = [" + parent.getText() + "]");
-            System.out.println("child.getText()  = [" + child.getText() + "]");
+            LOGGER.debug("parent.getText() = [" + parent.getText() + "]");
+            LOGGER.debug("child.getText()  = [" + child.getText() + "]");
         }
         else
         {
@@ -390,14 +319,12 @@ public class DefaultDataCollector implements DataCollector
             // Parent selector has two leaf children.
             final DCSelector selector0 = selectors.get(0);
             final DCSelector selector1 = selectors.get(1);
-            // final List<WebElement> elements0;
-            // elements0= selector0.getType()
-            // .findElements(parent, selector0.getValue());
             final List<WebElement> elements1 = selector1.getType()
                     .findElements(parent, selector1.getValue());
 
             final int size = elements1.size();
-            List<String> names = createNames(parent, selector0, size);
+            final List<String> names = createNames(parent, selector0, size);
+            LOGGER.debug("names = " + names);
 
             if (size > 0
                     && !rivalryData.getCandidatesList().contains(candidate))
@@ -407,31 +334,37 @@ public class DefaultDataCollector implements DataCollector
 
             for (int i = 0; i < size; i++)
             {
-                // final String name = elements0.get(i).getText();
-                String name = names.get(i);
-                final WebElement valueElement = elements1.get(i);
+                final String name = names.get(i);
 
-                final String categoryName = name;
-                Category category = rivalryData
-                        .findCategoryByName(categoryName);
-
-                if (category == null)
+                if (StringUtils.isNotEmpty(name))
                 {
-                    category = createCategory(categoryName);
-                    rivalryData.getCategoriesList().add(category);
-                }
+                    final WebElement valueElement = elements1.get(i);
 
-                final String criterionName = name;
-                Criterion criterion = rivalryData
-                        .findCriterionByName(criterionName);
-                if (criterion == null)
-                {
-                    criterion = createCriterion(criterionName, category);
-                    rivalryData.getCriteriaList().add(criterion);
-                }
+                    final String categoryName = name;
+                    Category category = rivalryData
+                            .findCategoryByName(categoryName);
 
-                final Double value = getValueStringParser().parse(valueElement);
-                candidate.putRating(criterion, value);
+                    if (category == null)
+                    {
+                        category = createCategory(categoryName);
+                        rivalryData.getCategoriesList().add(category);
+                    }
+
+                    final String criterionName = name;
+                    Criterion criterion = rivalryData
+                            .findCriterionByName(criterionName);
+                    if (criterion == null)
+                    {
+                        criterion = createCriterion(criterionName, category);
+                        rivalryData.getCriteriaList().add(criterion);
+                    }
+
+                    final Double value = getValueStringParser().parse(
+                            valueElement);
+                    LOGGER.debug("name = [" + name + "] value = [" + value
+                            + "]");
+                    candidate.putRating(criterion, value);
+                }
             }
         }
         else
