226e8674bdf3e6b996af6397a66746a1137cdd40
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-10 00:32:28 +0000

    Initial version.

---
+++ core/src/main/java/org/rivalry/core/datacollector/DataCollectorInjector.java
@@ -0,0 +1,55 @@
+//*****************************************************************************
+// Rivalry (http://code.google.com/p/rivalry)
+// Copyright (c) 2011 Rivalry.org
+// Admin rivalry@jeffreythompson.net
+//
+// See the file "LICENSE.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//*****************************************************************************
+package org.rivalry.core.datacollector;
+
+import org.rivalry.core.model.Category;
+import org.rivalry.core.model.Criterion;
+import org.rivalry.core.model.RivalryData;
+import org.rivalry.core.util.Provider;
+
+/**
+ * Defines methods required by a data collector injector.
+ */
+public interface DataCollectorInjector
+{
+    /**
+     * @return a new category provider.
+     */
+    Provider<Category> injectCategoryProvider();
+
+    /**
+     * @return a new criterion provider.
+     */
+    Provider<Criterion> injectCriterionProvider();
+
+    /**
+     * @return a new data collector.
+     */
+    DataCollector injectDataCollector();
+
+    /**
+     * @return a new data post processor.
+     */
+    DataPostProcessor injectDataPostProcessor();
+
+    /**
+     * @return a new name string parser.
+     */
+    NameStringParser injectNameStringParser();
+
+    /**
+     * @return a new rivalry data.
+     */
+    RivalryData injectRivalryData();
+
+    /**
+     * @return a new value string parser.
+     */
+    ValueStringParser injectValueStringParser();
+}
