b8ec41360e3016bf15499d6d65249800fb4ff9cc
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2011-12-24 23:07:22 +0000

    Changed to post-process the min and max number of players.

--- trunk/example/src/main/java/org/rivalry/example/boardgame/BoardgameDataPostProcessor.java
+++ trunk/example/src/main/java/org/rivalry/example/boardgame/BoardgameDataPostProcessor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.rivalry.core.datacollector.DefaultDataPostProcessor;
 import org.rivalry.core.model.Candidate;
 import org.rivalry.core.model.Criterion;
+import org.rivalry.core.model.DefaultCriterion;
 import org.rivalry.core.model.RivalryData;
 
 /**
@@ -14,6 +15,17 @@ public class BoardgameDataPostProcessor extends DefaultDataPostProcessor
 {
     @Override
     public void postProcess(final RivalryData rivalryData)
+    {
+        convertPlayingTime(rivalryData);
+        processNumPlayers(rivalryData);
+
+        super.postProcess(rivalryData);
+    }
+
+    /**
+     * @param rivalryData Rivalry data.
+     */
+    private void convertPlayingTime(final RivalryData rivalryData)
     {
         // Convert playing time into a number.
         final String criterionName = "Playing Time";
@@ -33,19 +45,113 @@ public class BoardgameDataPostProcessor extends DefaultDataPostProcessor
                 if (index >= 0)
                 {
                     playingTime = playingTime.substring(0, index);
-                    try
+                    final Integer time = parseInt(playingTime);
+
+                    if (time != null)
                     {
-                        final Integer time = Integer.parseInt(playingTime);
                         candidate.putValue(criterion, time);
-                    }
-                    catch (final NumberFormatException ignore)
-                    {
-                        // Nothing to do.
                     }
                 }
             }
         }
+    }
 
-        super.postProcess(rivalryData);
+    /**
+     * 
+     * @param rivalryData Rivalry data.
+     * @param criterionName Criterion name.
+     * 
+     * @return a new criterion.
+     */
+    private Criterion getCriterion(final RivalryData rivalryData,
+            final String criterionName)
+    {
+        Criterion answer = rivalryData.findCriterionByName(criterionName);
+
+        if (answer == null)
+        {
+            answer = new DefaultCriterion();
+            answer.setName(criterionName);
+
+            rivalryData.getCriteria().add(answer);
+        }
+
+        return answer;
+    }
+
+    /**
+     * @param value Value.
+     * 
+     * @return a new integer parsed from the given parameter.
+     */
+    private Integer parseInt(final String value)
+    {
+        Integer answer = null;
+
+        try
+        {
+            answer = Integer.parseInt(value);
+        }
+        catch (final NumberFormatException ignore)
+        {
+            // Nothing to do.
+        }
+
+        return answer;
+    }
+
+    /**
+     * @param rivalryData Rivalry data.
+     */
+    private void processNumPlayers(final RivalryData rivalryData)
+    {
+        final String criterionName = "# of Players";
+        final Criterion criterion = rivalryData
+                .findCriterionByName(criterionName);
+        final String minCriterionName = "Min Players";
+        final Criterion minCriterion = getCriterion(rivalryData,
+                minCriterionName);
+        final String maxCriterionName = "Max Players";
+        final Criterion maxCriterion = getCriterion(rivalryData,
+                maxCriterionName);
+        final List<Candidate> candidates = rivalryData.getCandidates();
+
+        for (final Candidate candidate : candidates)
+        {
+            final Object value = candidate.getValue(criterion);
+
+            if (value instanceof String)
+            {
+                final String numPlayersString = (String)value;
+
+                final String[] parts = numPlayersString.split(" ");
+
+                if (parts.length == 3)
+                {
+                    final String minPlayerString = parts[0];
+                    final Integer minPlayer = parseInt(minPlayerString);
+                    candidate.putValue(minCriterion, minPlayer);
+
+                    final String maxPlayerString = parts[2];
+                    final Integer maxPlayer = parseInt(maxPlayerString);
+                    candidate.putValue(maxCriterion, maxPlayer);
+                }
+                else
+                {
+                    final Integer numPlayers = parseInt(numPlayersString);
+                    candidate.putValue(minCriterion, numPlayers);
+                    candidate.putValue(maxCriterion, numPlayers);
+                }
+            }
+            else if (value instanceof Number)
+            {
+                candidate.putValue(minCriterion, ((Number)value).intValue());
+                candidate.putValue(maxCriterion, ((Number)value).intValue());
+            }
+
+            candidate.getValues().remove(criterion);
+        }
+
+        rivalryData.getCriteria().remove(criterion);
     }
 }
--- trunk/example/src/test/java/org/rivalry/example/boardgame/BoardgameDataPostProcessorTest.java
+++ trunk/example/src/test/java/org/rivalry/example/boardgame/BoardgameDataPostProcessorTest.java
@@ -25,11 +25,170 @@ public class BoardgameDataPostProcessorTest
     /** Criterion name. */
     private static final String CRITERION_NAME0 = "Playing Time";
 
+    /** Criterion name. */
+    private static final String CRITERION_NAME1 = "# of Players";
+
+    /** Criterion name. */
+    private static final String CRITERION_NAME2 = "Min Players";
+
+    /** Criterion name. */
+    private static final String CRITERION_NAME3 = "Max Players";
+
     /**
      * Test the <code>postProcess()</code> method.
      */
     @Test
-    public void postProcess()
+    public void postProcessNumPlayers0()
+    {
+        Candidate candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        final Criterion criterion = _rivalryData
+                .findCriterionByName(CRITERION_NAME1);
+        assertNotNull(criterion);
+        candidate0.putValue(criterion, "2 - 4");
+
+        final DataPostProcessor dataPostProcessor = new BoardgameDataPostProcessor();
+        dataPostProcessor.postProcess(_rivalryData);
+
+        final Criterion criterion2 = _rivalryData
+                .findCriterionByName(CRITERION_NAME2);
+        assertNotNull(criterion2);
+        final Criterion criterion3 = _rivalryData
+                .findCriterionByName(CRITERION_NAME3);
+        assertNotNull(criterion3);
+
+        candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        assertNotNull(candidate0.getValue(criterion2));
+        assertThat((Integer)candidate0.getValue(criterion2), is(2));
+        assertNotNull(candidate0.getValue(criterion3));
+        assertThat((Integer)candidate0.getValue(criterion3), is(4));
+    }
+
+    /**
+     * Test the <code>postProcess()</code> method.
+     */
+    @Test
+    public void postProcessNumPlayers1()
+    {
+        Candidate candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        final Criterion criterion = _rivalryData
+                .findCriterionByName(CRITERION_NAME1);
+        assertNotNull(criterion);
+        candidate0.putValue(criterion, "2 ? 5");
+
+        final DataPostProcessor dataPostProcessor = new BoardgameDataPostProcessor();
+        dataPostProcessor.postProcess(_rivalryData);
+
+        final Criterion criterion2 = _rivalryData
+                .findCriterionByName(CRITERION_NAME2);
+        assertNotNull(criterion2);
+        final Criterion criterion3 = _rivalryData
+                .findCriterionByName(CRITERION_NAME3);
+        assertNotNull(criterion3);
+
+        candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        assertNotNull(candidate0.getValue(criterion2));
+        assertThat((Integer)candidate0.getValue(criterion2), is(2));
+        assertNotNull(candidate0.getValue(criterion3));
+        assertThat((Integer)candidate0.getValue(criterion3), is(5));
+    }
+
+    /**
+     * Test the <code>postProcess()</code> method.
+     */
+    @Test
+    public void postProcessNumPlayersNaN()
+    {
+        Candidate candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        final Criterion criterion = _rivalryData
+                .findCriterionByName(CRITERION_NAME1);
+        assertNotNull(criterion);
+        candidate0.putValue(criterion, "N/A - something");
+
+        final DataPostProcessor dataPostProcessor = new BoardgameDataPostProcessor();
+        dataPostProcessor.postProcess(_rivalryData);
+
+        final Criterion criterion2 = _rivalryData
+                .findCriterionByName(CRITERION_NAME2);
+        assertNotNull(criterion2);
+        final Criterion criterion3 = _rivalryData
+                .findCriterionByName(CRITERION_NAME3);
+        assertNotNull(criterion3);
+
+        candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        assertNull(candidate0.getValue(criterion2));
+        assertNull(candidate0.getValue(criterion3));
+    }
+
+    /**
+     * Test the <code>postProcess()</code> method.
+     */
+    @Test
+    public void postProcessNumPlayersNull()
+    {
+        Candidate candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        final Criterion criterion = _rivalryData
+                .findCriterionByName(CRITERION_NAME1);
+        assertNotNull(criterion);
+
+        final DataPostProcessor dataPostProcessor = new BoardgameDataPostProcessor();
+        dataPostProcessor.postProcess(_rivalryData);
+
+        final Criterion criterion2 = _rivalryData
+                .findCriterionByName(CRITERION_NAME2);
+        assertNotNull(criterion2);
+        final Criterion criterion3 = _rivalryData
+                .findCriterionByName(CRITERION_NAME3);
+        assertNotNull(criterion3);
+
+        candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        assertNull(candidate0.getValue(criterion2));
+        assertNull(candidate0.getValue(criterion3));
+    }
+
+    /**
+     * Test the <code>postProcess()</code> method.
+     */
+    @Test
+    public void postProcessNumPlayersSingle()
+    {
+        Candidate candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        final Criterion criterion = _rivalryData
+                .findCriterionByName(CRITERION_NAME1);
+        assertNotNull(criterion);
+        candidate0.putValue(criterion, "2");
+
+        final DataPostProcessor dataPostProcessor = new BoardgameDataPostProcessor();
+        dataPostProcessor.postProcess(_rivalryData);
+
+        final Criterion criterion2 = _rivalryData
+                .findCriterionByName(CRITERION_NAME2);
+        assertNotNull(criterion2);
+        final Criterion criterion3 = _rivalryData
+                .findCriterionByName(CRITERION_NAME3);
+        assertNotNull(criterion3);
+
+        candidate0 = _rivalryData.getCandidates().get(0);
+        assertNotNull(candidate0);
+        assertNotNull(candidate0.getValue(criterion2));
+        assertThat((Integer)candidate0.getValue(criterion2), is(2));
+        assertNotNull(candidate0.getValue(criterion3));
+        assertThat((Integer)candidate0.getValue(criterion3), is(2));
+    }
+
+    /**
+     * Test the <code>postProcess()</code> method.
+     */
+    @Test
+    public void postProcessPlayingTime()
     {
         Candidate candidate0 = _rivalryData.getCandidates().get(0);
         assertNotNull(candidate0);
@@ -53,7 +212,7 @@ public class BoardgameDataPostProcessorTest
      * Test the <code>postProcess()</code> method.
      */
     @Test
-    public void postProcessNaN()
+    public void postProcessPlayingTimeNaN()
     {
         Candidate candidate0 = _rivalryData.getCandidates().get(0);
         assertNotNull(candidate0);
@@ -76,7 +235,7 @@ public class BoardgameDataPostProcessorTest
      * Test the <code>postProcess()</code> method.
      */
     @Test
-    public void postProcessNull()
+    public void postProcessPlayingTimeNull()
     {
         Candidate candidate0 = _rivalryData.getCandidates().get(0);
         assertNotNull(candidate0);
@@ -101,8 +260,13 @@ public class BoardgameDataPostProcessorTest
     {
         final TestData testData = new TestData();
         _rivalryData = testData.createRivalryData();
-        final Criterion criterion = new DefaultCriterion();
-        criterion.setName(CRITERION_NAME0);
-        _rivalryData.getCriteria().add(criterion);
+
+        final Criterion criterion0 = new DefaultCriterion();
+        criterion0.setName(CRITERION_NAME0);
+        _rivalryData.getCriteria().add(criterion0);
+
+        final Criterion criterion1 = new DefaultCriterion();
+        criterion1.setName(CRITERION_NAME1);
+        _rivalryData.getCriteria().add(criterion1);
     }
 }
