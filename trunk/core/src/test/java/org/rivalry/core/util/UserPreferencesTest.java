cc9efd562bc9b5c6e7640a091049b80cb2aa91b2
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2012-01-09 21:25:57 +0000

    Refactored user preference methods to separate the GUI related ones into another interface.

--- trunk/core/src/test/java/org/rivalry/core/util/UserPreferencesTest.java
+++ trunk/core/src/test/java/org/rivalry/core/util/UserPreferencesTest.java
@@ -2,13 +2,9 @@ package org.rivalry.core.util;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
-
-import javax.swing.RowSorter.SortKey;
-import javax.swing.SortOrder;
 
 import org.junit.After;
 import org.junit.Before;
@@ -26,32 +22,7 @@ public class UserPreferencesTest
     private static final String PREF_PREFIX = "test";
 
     /** User preferences. */
-    private final UserPreferences _userPreferences = new UserPreferences();
-
-    /**
-     * Test the <code>putColumnVisible()</code> method.
-     */
-    @Test
-    public void putColumnVisible()
-    {
-        final TestData testData = new TestData();
-        final List<Category> categories = testData.createCategories();
-        final List<Criterion> criteria = testData.createCriteria(categories);
-
-        final Criterion criterion = criteria.get(0);
-        final String columnName = criterion.getName();
-        Boolean result = _userPreferences.isColumnVisible(PREF_PREFIX,
-                columnName);
-        assertNotNull(result);
-        assertThat(result, is(Boolean.TRUE));
-
-        _userPreferences.putColumnVisible(PREF_PREFIX, columnName,
-                Boolean.FALSE);
-
-        result = _userPreferences.isColumnVisible(PREF_PREFIX, columnName);
-        assertNotNull(result);
-        assertThat(result, is(Boolean.FALSE));
-    }
+    private final UserPreferences _userPreferences = new UserPreferences(PREF_PREFIX);
 
     /**
      * Test the <code>putCriterionWeight()</code> method.
@@ -64,44 +35,16 @@ public class UserPreferencesTest
         final List<Criterion> criteria = testData.createCriteria(categories);
 
         final Criterion criterion = criteria.get(0);
-        Integer result = _userPreferences.getCriterionWeight(PREF_PREFIX,
+        Integer result = _userPreferences.getCriterionWeight(
                 criterion);
         assertNotNull(result);
         assertThat(result, is(0));
 
-        _userPreferences.putCriterionWeight(PREF_PREFIX, criterion, 1);
+        _userPreferences.putCriterionWeight( criterion, 1);
 
-        result = _userPreferences.getCriterionWeight(PREF_PREFIX, criterion);
+        result = _userPreferences.getCriterionWeight( criterion);
         assertNotNull(result);
         assertThat(result, is(1));
-    }
-
-    /**
-     * Test the <code>putSortKey()</code> method.
-     */
-    @Test
-    public void putSortKey()
-    {
-        SortKey result = _userPreferences.getSortKey(PREF_PREFIX);
-        assertNull(result);
-
-        int columnIndex = 0;
-        SortKey sortKey = new SortKey(columnIndex, SortOrder.ASCENDING);
-        _userPreferences.putSortKey(PREF_PREFIX, sortKey);
-
-        result = _userPreferences.getSortKey(PREF_PREFIX);
-        assertNotNull(result);
-        assertThat(result.getColumn(), is(columnIndex));
-        assertThat(result.getSortOrder(), is(SortOrder.ASCENDING));
-
-        columnIndex = 1;
-        sortKey = new SortKey(columnIndex, SortOrder.DESCENDING);
-        _userPreferences.putSortKey(PREF_PREFIX, sortKey);
-
-        result = _userPreferences.getSortKey(PREF_PREFIX);
-        assertNotNull(result);
-        assertThat(result.getColumn(), is(columnIndex));
-        assertThat(result.getSortOrder(), is(SortOrder.DESCENDING));
     }
 
     /**
@@ -110,9 +53,7 @@ public class UserPreferencesTest
     @Before
     public void setUp()
     {
-        _userPreferences.clearColumnVisibility(PREF_PREFIX);
-        _userPreferences.clearCriterionWeights(PREF_PREFIX);
-        _userPreferences.clearSortKey(PREF_PREFIX);
+        _userPreferences.clearCriterionWeights();
     }
 
     /**
@@ -121,8 +62,6 @@ public class UserPreferencesTest
     @After
     public void tearDown()
     {
-        _userPreferences.clearColumnVisibility(PREF_PREFIX);
-        _userPreferences.clearCriterionWeights(PREF_PREFIX);
-        _userPreferences.clearSortKey(PREF_PREFIX);
+        _userPreferences.clearCriterionWeights();
     }
 }
