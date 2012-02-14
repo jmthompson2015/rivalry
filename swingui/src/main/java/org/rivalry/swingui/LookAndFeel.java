ef30e95cbaeeab81d8ede200eed0eebc50b361b3
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2012-02-14 21:14:10 +0000

    Initial version.

---
+++ swingui/src/main/java/org/rivalry/swingui/LookAndFeel.java
@@ -0,0 +1,190 @@
+package org.rivalry.swingui;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import org.codehaus.plexus.util.StringUtils;
+
+/**
+ * Provides a user interface look and feel.
+ */
+public class LookAndFeel implements Comparable<LookAndFeel>
+{
+    /** Available look and feels. */
+    private static final Set<LookAndFeel> LOOK_AND_FEELS = new TreeSet<LookAndFeel>();
+
+    static
+    {
+        for (final LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+        {
+            final String name = info.getName();
+            final String lafClassName = info.getClassName();
+            boolean isCrossPlatform = true;
+
+            if (name.contains("Mac OS") || name.contains("Windows"))
+            {
+                isCrossPlatform = false;
+            }
+
+            final LookAndFeel laf = new LookAndFeel(name, lafClassName,
+                    isCrossPlatform);
+
+            LOOK_AND_FEELS.add(laf);
+        }
+    }
+
+    /**
+     * @param name Name.
+     * 
+     * @return the look and feel with the given name.
+     */
+    public static LookAndFeel findByName(final String name)
+    {
+        LookAndFeel answer = null;
+
+        if (StringUtils.isNotEmpty(name))
+        {
+            final Iterator<LookAndFeel> iter = LOOK_AND_FEELS.iterator();
+
+            while (answer == null && iter.hasNext())
+            {
+                final LookAndFeel laf = iter.next();
+
+                if (name.equals(laf.getName()))
+                {
+                    answer = laf;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    /**
+     * @return the available look and feels.
+     */
+    public static final Set<LookAndFeel> getAvailableLookAndFeels()
+    {
+        return LOOK_AND_FEELS;
+    }
+
+    /**
+     * @return the default look and feel.
+     */
+    public static final LookAndFeel getDefaultLookAndFeel()
+    {
+        LookAndFeel answer = findByName("Nimbus");
+
+        if (answer == null)
+        {
+            answer = findByName("Metal");
+        }
+
+        return answer;
+    }
+
+    /** Name. */
+    private final String _name;
+
+    /** Class name. */
+    private final String _className;
+
+    /** Flag indicating if this is cross platform. */
+    private final boolean _isCrossPlatform;
+
+    /**
+     * Construct this object with the given parameters.
+     * 
+     * @param name Name.
+     * @param className Class name.
+     * @param isCrossPlatform Flag indicating if this is cross platform.
+     */
+    public LookAndFeel(final String name, final String className,
+            final boolean isCrossPlatform)
+    {
+        _name = name;
+        _className = className;
+        _isCrossPlatform = isCrossPlatform;
+    }
+
+    /**
+     * Apply this look and feel.
+     */
+    public void apply()
+    {
+        try
+        {
+            UIManager.setLookAndFeel(getClassName());
+        }
+        catch (final ClassNotFoundException e)
+        {
+            handleException(e);
+        }
+        catch (final InstantiationException e)
+        {
+            handleException(e);
+        }
+        catch (final IllegalAccessException e)
+        {
+            handleException(e);
+        }
+        catch (final UnsupportedLookAndFeelException e)
+        {
+            handleException(e);
+        }
+    }
+
+    @Override
+    public int compareTo(final LookAndFeel another)
+    {
+        int answer = 0;
+
+        if (another == null)
+        {
+            answer = -1;
+        }
+        else
+        {
+            answer = getName().compareTo(another.getName());
+        }
+
+        return answer;
+    }
+
+    /**
+     * @return the className
+     */
+    public String getClassName()
+    {
+        return _className;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName()
+    {
+        return _name;
+    }
+
+    /**
+     * @return the isCrossPlatform
+     */
+    public boolean isCrossPlatform()
+    {
+        return _isCrossPlatform;
+    }
+
+    /**
+     * @param e Exception.
+     */
+    private void handleException(final Exception e)
+    {
+        System.out.println(e.getClass().getName() + ": " + e.getMessage());
+    }
+}
---
+++ swingui/src/test/java/org/rivalry/swingui/DefaultUIUserPreferencesTest.java
@@ -0,0 +1,49 @@
+package org.rivalry.swingui;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Provides tests for the <code>DefaultUIUserPreferences</code> class.
+ */
+public class DefaultUIUserPreferencesTest
+{
+    /** User preferences. */
+    private final UIUserPreferences _userPreferences = new DefaultUIUserPreferences();
+
+    /**
+     * Test the <code>putLookAndFeel()</code> method.
+     */
+    @Test
+    public void putLookAndFeel()
+    {
+        final LookAndFeel laf = LookAndFeel.getDefaultLookAndFeel();
+        _userPreferences.putLookAndFeel(laf);
+        final LookAndFeel result = _userPreferences.getLookAndFeel();
+        assertNotNull(result);
+        assertThat(result.getName(), is("Nimbus"));
+    }
+
+    /**
+     * Set up the test.
+     */
+    @Before
+    public void setUp()
+    {
+        _userPreferences.clearLookAndFeel();
+    }
+
+    /**
+     * Tear down the test.
+     */
+    @After
+    public void tearDown()
+    {
+        _userPreferences.clearLookAndFeel();
+    }
+}
