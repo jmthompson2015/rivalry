f97570c1854e7028327c733a52a5e07c0b5c6769
jmthompson2007 <jmthompson2007@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2012-03-27 20:17:43 +0000

    Moved.

--- core/src/main/bin/jfabber.sh
+++ core/src/main/bin/jfabber.sh
@@ -1,25 +1,31 @@
 #! /bin/bash
 
 USER_DIR=/Users/jmthompson
-JFAB_DIR=${USER_DIR}/Dropbox/SoftwareDev/JavaProjects/jfab/trunk/core
-BASE_DIR=${USER_DIR}/Dropbox/SoftwareDev/JavaProjects/rivalry/core
+JFAB_DIR=${USER_DIR}/Dropbox/SoftwareDev/JavaProjects/jfab/trunk
+BASE_DIR=${USER_DIR}/Dropbox/SoftwareDev/JavaProjects/rivalry
 LIB_DIR=${USER_DIR}/.m2/repository
 
 INPUT_FILE=src/main/resources/RivalryModel.xml
-OUTPUT_DIR=${BASE_DIR}/target/generated-sources/jfab
+MODULE=core
+OUTPUT_DIR=${BASE_DIR}/${MODULE}/target/generated-sources/jfab
 
-CP=${BASE_DIR}/../classes
-CP=${OUTPUT_DIR}:${CP}
-CP=${JFAB_DIR}/../classes:${CP}
+echo "INPUT_FILE =" ${INPUT_FILE}
+echo "MODULE     =" ${MODULE}
+echo "OUTPUT_DIR =" ${OUTPUT_DIR}
+
+CP=${OUTPUT_DIR}
+CP=${BASE_DIR}/${MODULE}/target/classes:${CP}
+CP=${JFAB_DIR}/classes:${CP}
 CP=${LIB_DIR}/commons-cli/commons-cli/1.2/commons-cli-1.2.jar:${CP}
 CP=${LIB_DIR}/commons-collections/commons-collections/3.2.1/commons-collections-3.2.1.jar:${CP}
 CP=${LIB_DIR}/commons-io/commons-io/2.0.1/commons-io-2.0.1.jar:${CP}
 CP=${LIB_DIR}/commons-lang/commons-lang/2.6/commons-lang-2.6.jar:${CP}
 CP=${LIB_DIR}/org/slf4j/slf4j-api/1.6.1/slf4j-api-1.6.1.jar:${CP}
+CP=${LIB_DIR}/org/slf4j/slf4j-simple/1.6.1/slf4j-simple-1.6.1.jar:${CP}
 CP=${LIB_DIR}/org/javaruntype/javaruntype/1.2/javaruntype-1.2.jar:${CP}
 CP=${LIB_DIR}/org/antlr/antlr-runtime/3.1.2/antlr-runtime-3.1.2.jar:${CP}
 
 rm -rf ${OUTPUT_DIR}/*
 
-java -cp ${CP} org.jfab.core.JFabberMain -f src/main/resources/RivalryModel.xml -d ${OUTPUT_DIR}
+java -cp ${CP} org.jfab.core.JFabberMain -f ${INPUT_FILE} -d ${OUTPUT_DIR}
 
