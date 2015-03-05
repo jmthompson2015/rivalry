7794c9f19c8b9c551ccb1ec1a9f4a8ac1a0a4ac2
jmthompson2007@gmail.com <jmthompson2007@gmail.com@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2015-03-05 20:02:08 +0000

    Fixed a problem reading in the Candidate's values map.

--- core/src/test/java/org/rivalry/core/model/io/RivalryDataReaderTest.java
+++ core/src/test/java/org/rivalry/core/model/io/RivalryDataReaderTest.java
@@ -18,10 +18,14 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.List;
+import java.util.Map.Entry;
 
 import org.junit.Test;
 import org.rivalry.core.model.Candidate;
+import org.rivalry.core.model.Category;
 import org.rivalry.core.model.Criterion;
+import org.rivalry.core.model.CriterionMap;
 import org.rivalry.core.model.RivalryData;
 
 /**
@@ -45,16 +49,83 @@ public class RivalryDataReaderTest
 
         // Run.
         final RivalryData result = rivalryDataReader.read(reader);
-        // System.out.println("result =\n" + result);
+        System.out.println("result =\n" + result);
 
         // Verify.
         assertNotNull(result);
-        assertThat(result.getCategories().size(), is(0));
-        assertThat(result.getCriteria().size(), is(4));
-        assertThat(result.getCandidates().size(), is(1));
+        verifyCategories(result.getCategories());
+        verifyCriteria(result.getCriteria());
+        verifyCandidates(result);
+    }
+
+    /**
+     * @param rivalryData Rivalry data.
+     */
+    private void verifyCandidates(final RivalryData rivalryData)
+    {
+        final List<Candidate> candidates = rivalryData.getCandidates();
+        assertNotNull(candidates);
+        assertThat(candidates.size(), is(1));
 
         {
-            final Criterion criterion = result.getCriteria().get(0);
+            final Candidate candidate = candidates.get(0);
+            assertNotNull(candidate);
+            assertThat(candidate.getName(), is("Mac Mini Late 2014 1.4 GHz"));
+            assertNull(candidate.getDescription());
+            assertThat(candidate.getPage(), is("http://store.apple.com/us/buy-mac/mac-mini"));
+
+            final CriterionMap map = candidate.getValues();
+            System.out.println("map.size() = " + map.size());
+            for (final Entry<String, Object> entry : map.entrySet())
+            {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
+
+            assertThat((String)candidate.getValue(rivalryData.findCriterionByName("release")), is("2014/10"));
+            assertThat((Double)candidate.getValue(rivalryData.findCriterionByName("processorSpeed")), is(1.4));
+            assertThat((String)candidate.getValue(rivalryData.findCriterionByName("processor")),
+                    is("dual-core Intel Core i5"));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("ram")), is(4));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("harddrive")), is(500));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("harddriveSpeed")), is(5400));
+            assertNull(candidate.getValue(rivalryData.findCriterionByName("flashStorage")));
+            assertThat((String)candidate.getValue(rivalryData.findCriterionByName("graphicsCard")),
+                    is("Intel HD Graphics 5000"));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("graphicsRam")), is(1024));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("price")), is(499));
+            assertThat((Integer)candidate.getValue(rivalryData.findCriterionByName("geekbench3Score")), is(5382));
+        }
+    }
+
+    /**
+     * @param categories Categories.
+     */
+    private void verifyCategories(final List<Category> categories)
+    {
+        assertNotNull(categories);
+        assertThat(categories.size(), is(0));
+    }
+
+    /**
+     * @param criteria Criteria.
+     */
+    private void verifyCriteria(final List<Criterion> criteria)
+    {
+        assertNotNull(criteria);
+        assertThat(criteria.size(), is(11));
+
+        int i = 0;
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("release"));
+            assertThat(criterion.getDescription(), is("Release"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
             assertNotNull(criterion);
             assertThat(criterion.getName(), is("processorSpeed"));
             assertThat(criterion.getDescription(), is("Processor Speed (GHz)"));
@@ -62,11 +133,77 @@ public class RivalryDataReaderTest
         }
 
         {
-            final Candidate candidate = result.getCandidates().get(0);
-            assertNotNull(candidate);
-            assertThat(candidate.getName(), is("Mac Mini Late 2014 1.4 GHz"));
-            assertNull(candidate.getDescription());
-            assertThat(candidate.getPage(), is("http://store.apple.com/us/buy-mac/mac-mini"));
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("processor"));
+            assertThat(criterion.getDescription(), is("Processor"));
+            assertTrue(criterion.isAutoMinMax());
         }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("ram"));
+            assertThat(criterion.getDescription(), is("RAM (GB)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("harddrive"));
+            assertThat(criterion.getDescription(), is("Harddrive (GB)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("harddriveSpeed"));
+            assertThat(criterion.getDescription(), is("Harddrive Speed (rpm)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("flashStorage"));
+            assertThat(criterion.getDescription(), is("Flash Storage (GB)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("graphicsCard"));
+            assertThat(criterion.getDescription(), is("Graphics Card"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("graphicsRam"));
+            assertThat(criterion.getDescription(), is("Graphics RAM (MB)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("price"));
+            assertThat(criterion.getDescription(), is("Price ($)"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        {
+            final Criterion criterion = criteria.get(i++);
+            assertNotNull(criterion);
+            assertThat(criterion.getName(), is("geekbench3Score"));
+            assertThat(criterion.getDescription(), is("Geekbench 3 Score"));
+            assertTrue(criterion.isAutoMinMax());
+        }
+
+        assertThat(i, is(11));
     }
 }
