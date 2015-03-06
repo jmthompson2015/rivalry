ef513e98badb35a2cb221623ca8fa1f9405f8b6e
jmthompson2007@gmail.com <jmthompson2007@gmail.com@89a68bfa-32a8-363b-c141-4bbefb83ef0d>
2015-03-06 18:15:48 +0000

    Removed property change code from Entity.

--- core/src/main/java/org/rivalry/core/model/Entity.java
+++ core/src/main/java/org/rivalry/core/model/Entity.java
@@ -8,31 +8,11 @@
 //*****************************************************************************
 package org.rivalry.core.model;
 
-import java.beans.PropertyChangeListener;
-
 /**
  * Defines methods required by an entity for a decision making application.
  */
 public interface Entity
 {
-    /**
-     * Add a <code>PropertyChangeListener</code> to the listener list. The listener is registered for all properties.
-     * This method will only add the listener if it is not already registered.
-     *
-     * @param listener The <code>PropertyChangeListener</code> to be added.
-     */
-    void addPropertyChangeListener(PropertyChangeListener listener);
-
-    /**
-     * Add a <code>PropertyChangeListener</code> for a specific property. The listener will be invoked only when a call
-     * on <code>
-     * firePropertyChange</code> names that specific property.
-     *
-     * @param propertyName The name of the property to listen on.
-     * @param listener The <code>PropertyChangeListener</code> to be added.
-     */
-    void addPropertyChangeListener(String propertyName, PropertyChangeListener listener);
-
     /**
      * @return description
      */
@@ -42,22 +22,6 @@ public interface Entity
      * @return name
      */
     String getName();
-
-    /**
-     * Remove a <code>PropertyChangeListener</code> from the listener list. This removes a
-     * <code>PropertyChangeListener</code> that was registered for all properties.
-     *
-     * @param listener The <code>PropertyChangeListener</code> to be removed.
-     */
-    void removePropertyChangeListener(PropertyChangeListener listener);
-
-    /**
-     * Remove a <code>PropertyChangeListener</code> for a specific property.
-     *
-     * @param propertyName The name of the property to listen on.
-     * @param listener The <code>PropertyChangeListener</code> to be removed.
-     */
-    void removePropertyChangeListener(String propertyName, PropertyChangeListener listener);
 
     /**
      * @param descriptionIn description
